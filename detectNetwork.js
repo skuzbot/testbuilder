63f2dc3253e40454af6e9b9c21183cc307f73fb9
Steven Duncan <steven.k.duncan@gmail.com>
2018-08-29 13:02:05 -0500

    Add Discover and Maestro
    Discover prefix: 6011, 644-649, 65 length 16, 19
    Maestro prefix 5018, 5020, 5038, 6304 length 12-19

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,14 +1,18 @@
 //Detects Credit Card Network from Card Number
+// Diners club        prefix 38, 39                 length 14
+// American Express   prefix 34, 37                 lenfth 15
+// Visa               prefix 4                      length 13, 16, 19
+// MasterCard         prefix 51-55                  length 16
+// Discover           prefix of 6011, 644-649, 65   length 16, 19.
+// Maestro            prefix 5018, 5020, 5038, 6304 length 12-19.
 
-// Nice work! Extend your function to support two popular networks, Visa and Mastercard:
-
-// Visa always has a prefix of 4 and a length of 13, 16, or 19.
-// MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
-
+//Note for morning: I may want to make an array for each card and include prefixes as elements. might be easier.
 
 var detectNetwork = function(cardNumber) {
-  var firstDigit = (''+cardNumber)[0];	
-  var firstTwoDigits = (''+cardNumber)[0]+(''+cardNumber)[1];
+  var firstDigit = cardNumber.slice(0, 1);	
+  var firstTwoDigits = cardNumber.slice(0, 2);
+  var firstThreeDigits = cardNumber.slice(0, 3);
+  var firstFourDigits = cardNumber.slice(0, 4);
   var cardLength = cardNumber.length;
   if ((firstTwoDigits === '38', '39') && (cardLength === 14)) {
   	return 'Diner\'s Club';
@@ -22,6 +26,13 @@ var detectNetwork = function(cardNumber) {
   else if ((firstTwoDigits === '51', '52', '53', '54', '55') && (cardLength === 16)) {
   	return 'MasterCard';
   }
+  else if ( ((firstFourDigits === 6011) || ((firstThreeDigits >= 644) && (firstThreeDigits <= 649)) || (firstTwoDigits === 65)) && (cardLength === 16, 19) ) {
+    return 'Discover';
+  }
+  else if ((firstFourDigits === 5018, 5020, 5038, 6304) && ((cardLength >= 12) && (cardLength <= 19))) {
+    return 'Maestro'; 
+  }
 };
 
+detectNetwork('3934567890123');
 
