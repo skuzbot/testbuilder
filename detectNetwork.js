15e5128591698ed34dc79992f72806b5121dafdf
Steven Duncan <steven.k.duncan@gmail.com>
2018-08-28 23:56:46 -0500

    Add Visa and MasterCard - Add Parens
    Visa rules: first digit is 4 and length of 13, 16, 19
    MasterCard rules: first two digits are 51-55 and length is 16
    Add parens around if conditions

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,27 +1,27 @@
-// Given a credit card number, this function should return a string with the
-// name of a network, like 'MasterCard' or 'American Express'
-// Example: detectNetwork('343456789012345') should return 'American Express'
+//Detects Credit Card Network from Card Number
 
-// How can you tell one card network from another? Easy!
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
+// Nice work! Extend your function to support two popular networks, Visa and Mastercard:
+
+// Visa always has a prefix of 4 and a length of 13, 16, or 19.
+// MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  var firstDigit = (''+cardNumber)[0];	
   var firstTwoDigits = (''+cardNumber)[0]+(''+cardNumber)[1];
   var cardLength = cardNumber.length;
-  if (firstTwoDigits === 38, 39 && cardLength === 14) {
+  if ((firstTwoDigits === '38', '39') && (cardLength === 14)) {
   	return 'Diner\'s Club';
   }
-  else if (firstTwoDigits === 34, 37 && cardLength === 15) {
+  else if ((firstTwoDigits === '34', '37') && (cardLength === 15)) {
   	return 'American Express';
   }
-
+  else if ((firstDigit === '4') && (cardLength === 13, 16, 19)) {
+  	return 'Visa';
+  }
+  else if ((firstTwoDigits === '51', '52', '53', '54', '55') && (cardLength === 16)) {
+  	return 'MasterCard';
+  }
 };
 
-detectNetwork('39345678901234');
+
