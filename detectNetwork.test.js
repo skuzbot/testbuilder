8507d837fee6077f07138fabf6e6393dd2c0bed1
Steven Duncan <steven.k.duncan@gmail.com>
2018-08-29 15:38:08 -0500

    Fix Diners Club, Amex test
    added a digit to Diners Club test so test string is 14 length
    changed isTrue to !isTrue to Amex test

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -53,7 +53,7 @@ describe('Diner\'s Club', function() {
   });
 
   it('has a prefix of 39 and a length of 14', function() {
-    if (detectNetwork('39345567890123') !== 'Diner\'s Club') {
+    if (detectNetwork('3934567890123') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
  
@@ -64,7 +64,7 @@ describe('American Express', function() {
   // It can get annoying to keep typing the if/throw, so here is a
   // helper function to throw an error if the input statement isn't true. 
   var assert = function(isTrue) {
-    if(!isTrue) {
+    if(isTrue) {
       throw new Error('Test failed');
     }
  
